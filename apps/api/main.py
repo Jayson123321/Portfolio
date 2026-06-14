474c58c34994d4978f90bcb9c1445ff65abc627f
Jayson <jayson.haverkamp@hva.nl>
2026-06-14 23:17:29 +0200

    Add GitHub API integration and update dependencies for requests

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,7 +1,22 @@
-﻿from fastapi import FastAPI
+﻿import os
+import requests
+from dotenv import load_dotenv
+from models import ProjectModel
+from fastapi import FastAPI
+
+load_dotenv() 
 
 app = FastAPI()
+
+github_base_url = os.getenv("GITHUB_BASE_URL")
 
 @app.get("/")
 def root():
     return {"message": "Portfolio API"}
+
+@app.get("/githubRepos")
+def get_github_repos():
+    response = requests.get(github_base_url)
+    data = response.json()
+    projects = [ProjectModel.from_github(repo) for repo in data]
+    return projects
